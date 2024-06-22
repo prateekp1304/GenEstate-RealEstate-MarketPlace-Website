3e93e73e409f345ac1a718a3579bb3bbfafe9cdc
Prateek Patel <prateekpatel307@gmail.com>
2024-06-22 16:37:41 +0530

    add pre installation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,11 +25,12 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Gen</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
-
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Gen</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
         <form
           onSubmit={handleSubmit}
           className="bg-slate-100 p-3 rounded-lg flex items-center"
@@ -37,7 +38,7 @@ export default function Header() {
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64 "
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -64,7 +65,7 @@ export default function Header() {
                 alt="profile"
               />
             ) : (
-              <li className=" text-slate-700 hover:underline">Sign in</li>
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
             )}
           </Link>
         </ul>
